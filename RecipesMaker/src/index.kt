6dcb2c2b7369d9c234bc2a1230630506d15a5861
sebasvz1903 <44046478+sebasvz1903@users.noreply.github.com>
2019-12-22 00:46:32 -0500

    Add files via upload

--- RecipesMaker/src/index.kt
+++ RecipesMaker/src/index.kt
@@ -18,7 +18,7 @@ fun main() {
     3. Salir.
     """.trimIndent()
 
-    var nombre: String = "Escribe el nombre de tu receta (Ejemplo: Papas Fritas): " +
+    val nombre: String = "Escribe el nombre de tu receta (Ejemplo: Papas Fritas): " +
             "\nO escribe V para VOLVER al menú principal"
 
     val a = "Agua"
@@ -43,9 +43,68 @@ fun main() {
 
     var situacion:Int? = menu
 
+    val recetas = mutableListOf("\tRECETAS: \n")
+
 //      ACCIONES
 
-    val recetas = mutableListOf("Recetas: \n\n")
+    fun viewRecipes(){
+        println(recetas)
+    }
+
+    fun makeRecipe(){
+        var ingrediente = true
+
+        println (nombre)
+
+        when (val name:String? = readLine()) {
+            "V" -> situacion = menu
+            else -> recetas.add("\n\n$name: \n")
+        }
+
+        println(guardado)
+
+        ingredientes@ while (ingrediente) {
+
+            var posicion = 1
+
+            for (element in ingredientes) {
+                println("$posicion $element")
+                posicion = posicion.inc()
+            }
+
+            println("\n$add")
+            val responseI:String? = readLine()
+
+            when (responseI?.toInt()) {
+                1 -> recetas.add("- $a -")
+                2 -> recetas.add("- $l -")
+                3 -> recetas.add("- $c -")
+                4 -> recetas.add("- $v -")
+                5 -> recetas.add("- $f -")
+                6 -> recetas.add("- $ce -")
+                7 -> recetas.add("- $h -")
+                8 -> recetas.add("- $ac -")
+                else -> println(ingresa)
+            }
+
+            println (guardado)
+            println(addOtro)
+
+            val responseO:String? = readLine()
+
+            if(responseO == "A"){
+                continue@ingredientes
+            } else if(responseO == "G"){
+                println(recetas)
+                ingrediente = false
+            } else {
+                println(ingresa)
+                println(recetas)
+                continue@ingredientes
+            }
+
+        }
+    }
 
 principal@  while (!(situacion?.equals(salir) ?: (false))) {
 
@@ -54,68 +113,18 @@ principal@  while (!(situacion?.equals(salir) ?: (false))) {
 
                 val response:String? = readLine()
                 situacion = response?.toInt()
-
-
-
+    
                 if (situacion?.equals(hacer) ?: (false)) {
 
-                    val ingrediente = true
-
-                    println (nombre)
-
-                    when (val name:String? = readLine()) {
-                        "V" -> continue@principal
-                        else -> recetas.add("$name: \n")
-                    }
-
-                    println(guardado)
-
-ingredientes@ while (ingrediente) {
-
-                var posicion = 1
-
-                for (element in ingredientes) {
-                    println("$posicion $element")
-                    posicion = posicion.inc()
-                }
-
-                println("\n$add")
-                val responseI:String? = readLine()
-
-                when (val i: Int? = responseI?.toInt()) {
-                    1 -> recetas.add("- $a -")
-                    2 -> recetas.add("- $l -")
-                    3 -> recetas.add("- $c -")
-                    4 -> recetas.add("- $v -")
-                    5 -> recetas.add("- $f -")
-                    6 -> recetas.add("- $ce -")
-                    7 -> recetas.add("- $h -")
-                    8 -> recetas.add("- $ac -")
-                    else -> println(ingresa)
-                }
-
-                println (guardado)
-                println(addOtro)
-
-                val responseO:String? = readLine()
-
-                if(responseO == "A"){
-                    continue@ingredientes
-                } else if(responseO == "G"){
-                    println(recetas)
-                    continue@principal
-                } else {
-                    println(ingresa)
-                    println(recetas)
-                    continue@ingredientes
-                }
-
-            }
+                    makeRecipe()
 
         } else if (situacion?.equals(ver) ?: (false)){
-                    println(recetas)
+                    viewRecipes()
+                } else{
+
+                    println(ingresa)
+                    continue@principal
+
                 }
-
     }
-
 }
